c7a98d982626dddde1e327841670519a9d7feac3
Dane Madsen <dane_madsen@hotmail.com>
2023-04-28 21:54:53 +1000

    Initial

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import LandingPage from './pages/LandingPage.js';
+import MoviesPage from './pages/MoviesPage.js';
+import MoviePage from './pages/MoviePage.js';
+import PersonPage from './pages/PersonPage.js';
+import ProfilePage from './pages/ProfilePage.js';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/userprofile">User Profile</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/movies" element={<MoviesPage />} />
+          <Route exact path="/movies/:id" element={<MoviePage />} />
+          <Route exact path="/people/:id" element={<PersonPage />} />
+          <Route exact path="/userprofile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to My Movie App</h1>
+      <p>This app allows you to search for movies and learn more about the people involved in making them. Get started by clicking on the Movies link above.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
