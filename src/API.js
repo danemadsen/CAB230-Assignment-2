b9e31016209ac0090026a5dabda37f09c4915afc
Dane Madsen <dane_madsen@hotmail.com>
2023-04-30 16:19:51 +1000

    Update API

--- src/API.js
+++ src/API.js
@@ -3,24 +3,44 @@ const API_ADDRESS = 'http://sefdb02.qut.edu.au:3000';
 export const fetchMovies = async (year, title, page = 1) => {
   try {
     const response = await fetch(`${API_ADDRESS}/movies/search?year=${year}&title=${title}&page=${page}`);
+    
+    if (response.status === 429) {
+      throw new Error('Too many requests, please try again later.');
+    }
+
     const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
     console.log(data); // log the response data to see what's returned
     return data; // Return the whole data object to access pagination data as well
   } catch (error) {
     console.error(error);
-    return { data: [], pagination: {} };
+    return { data: [], pagination: {}, error: error.message };
   }
 };
 
 export const fetchMovie = async (id) => {
   try {
     const response = await fetch(`${API_ADDRESS}/movies/data/${id}`);
+    
+    if (response.status === 429) {
+      throw new Error('Too many requests, please try again later.');
+    }
+
     const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
     console.log(data); // log the response data to see what's returned
     return data;
   } catch (error) {
     console.error(error);
-    return null;
+    return { error: error.message };
   }
 };
 
@@ -35,3 +55,31 @@ export const fetchPerson = async (id) => {
     return null;
   }
 }
+
+export const registerUser = async (email, password) => {
+  try {
+    const response = await fetch(`${API_ADDRESS}/user/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.status === 429) {
+      throw new Error('Too many requests, please try again later.');
+    }
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { success: true, message: data.message };
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: error.message };
+  }
+};
+
