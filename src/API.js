838c05cbde4e868e69bd2361f9fdab378805bc71
Dane Madsen <dane_madsen@hotmail.com>
2023-04-30 12:06:04 +1000

    f

--- src/API.js
+++ src/API.js
@@ -1,14 +1,14 @@
 const API_ADDRESS = 'http://sefdb02.qut.edu.au:3000';
 
-export const fetchMovies = async (year, title) => {
+export const fetchMovies = async (year, title, page = 1) => {
   try {
-    const response = await fetch(`${API_ADDRESS}/movies/search?year=${year}&title=${title}`);
+    const response = await fetch(`${API_ADDRESS}/movies/search?year=${year}&title=${title}&page=${page}`);
     const data = await response.json();
     console.log(data); // log the response data to see what's returned
-    return data.data; // Access the correct property containing the movie array
+    return data; // Return the whole data object to access pagination data as well
   } catch (error) {
     console.error(error);
-    return [];
+    return { data: [], pagination: {} };
   }
 };
 
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { fetchMovies } from '../API';
 import { useNavigate } from 'react-router-dom';
+import '../App.css';
 
 function MoviesPage() {
   const [movies, setMovies] = React.useState([]);
-  const [year, setYear] = React.useState('');
+  const [year, setYear] = React.useState('1996');
   const [title, setTitle] = React.useState('');
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [totalPages, setTotalPages] = React.useState(null);
   const navigate = useNavigate();
 
   const handleYearChange = (event) => {
@@ -18,35 +21,55 @@ function MoviesPage() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetchMovies(year, title)
-      .then(results => setMovies(results))
+    fetchMovies(year, title, currentPage)
+      .then(({ data, pagination }) => {
+        setMovies(data);
+        setTotalPages(pagination.lastPage);
+      })
       .catch(error => console.log(error));
   };
 
   React.useEffect(() => {
-    fetchMovies('1996', title)
-      .then(results => setMovies(results))
+    fetchMovies(year, title, currentPage)
+      .then(({ data, pagination }) => {
+        setMovies(data);
+        setTotalPages(pagination.lastPage);
+      })
       .catch(error => console.log(error));
-  }, [title]);
+  }, [year, title, currentPage]);
 
   const handleMovieRowClick = (imdbID) => {
     navigate(`/movies/data/${imdbID}`);
   };
 
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div>
-      <h1>Movies</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title-input">Search by Title:</label>
-        <input id="title-input" type="text" value={title} onChange={handleTitleChange} />
-        <br />
-        <label htmlFor="year-input">Search by Year:</label>
-        <input id="year-input" type="text" value={year} onChange={handleYearChange} />
-        <br />
+    <div className="movies-page">
+      <h1 style={{ textAlign: 'center' }}>Movies</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <div>
+          <label htmlFor="title-input">Search by Title:</label>
+          <input id="title-input" type="text" value={title} onChange={handleTitleChange} />
+        </div>
+        <div>
+          <label htmlFor="year-input">Search by Year:</label>
+          <input id="year-input" type="text" value={year} onChange={handleYearChange} />
+        </div>
         <button type="submit">Search</button>
       </form>
       {movies.length > 0 ? (
-        <table>
+        <table className="movies-table">
           <thead>
             <tr>
               <th>Title</th>
@@ -73,8 +96,19 @@ function MoviesPage() {
       ) : (
         <p>Loading....</p>
       )}
+      <div className="pagination">
+        <button className="prev-button" onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <span>
+          {currentPage} / {totalPages}
+        </span>
+        <button className="next-button" onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
-  );
+  );  
 }
 
 export default MoviesPage;
