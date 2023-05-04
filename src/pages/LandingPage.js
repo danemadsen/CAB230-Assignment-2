8a730569ab9df2f198eee3753acbbd78b2725dca
Dane Madsen <dane_madsen@hotmail.com>
2023-05-04 22:47:01 +1000

    Update landingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getMovies, getMovie } from '../API';
+import { Carousel } from 'react-responsive-carousel';
+import { useNavigate } from 'react-router-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function LandingPage() {
+  const [topMovies, setTopMovies] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchTopMovies = async () => {
+      try {
+        const { data } = await getMovies(2022, '', 1);
+
+        const sortedMovies = data.sort(
+          (a, b) => parseFloat(b.imdbRating) - parseFloat(a.imdbRating)
+        );
+
+        const topSixMovies = sortedMovies.slice(0, 6);
+
+        const fetchMoviesWithPoster = topSixMovies.map(async (movie) => {
+          const movieDetails = await getMovie(movie.imdbID);
+          return { ...movie, ...movieDetails }; // Combine list movie and individual movie objects
+        });
+
+        Promise.all(fetchMoviesWithPoster).then((moviesWithPoster) => {
+          setTopMovies(moviesWithPoster);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchTopMovies();
+  }, []);
+
+  const handleMovieClick = (movieID) => {
+    navigate(`/movies/data/${movieID}`);
+  };
+
   return (
     <div>
       <h1>University Movie Data Base</h1>
-      <p>This app allows you to search for movies and learn more about the people involved in making them. Get started by clicking on the Movies link above.</p>
+      <p>
+        This app allows you to search for movies and learn more about the people
+        involved in making them. Get started by clicking on the Movies link above.
+      </p>
+      {topMovies.length > 0 && (
+        <Carousel
+          showThumbs={false}
+          infiniteLoop
+          autoPlay
+          showStatus={false}
+          centerMode
+          centerSlidePercentage={50}
+          emulateTouch
+          onClickItem={(index) => handleMovieClick(topMovies[index].imdbID)}
+        >
+          {topMovies.map((movie) => (
+            <div className="carousel-slide" key={movie.imdbID}>
+              <img className="poster-image" src={movie.poster} alt={movie.title} />
+              <p className="legend">{movie.title}</p>
+            </div>
+          ))}
+        </Carousel>
+      )}
     </div>
   );
 }
