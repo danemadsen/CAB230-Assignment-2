7dddb303a2bfd4750590f53ec20b4d6dd857957c
Dane Madsen <dane_madsen@hotmail.com>
2023-05-09 12:15:54 +1000

    Infinite scrolling

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getMovies } from '../API';
 import { MovieCell } from '../components/MovieCell';
@@ -23,6 +23,9 @@ function useDebounce(value, delay) {
 function MoviesPage() {
   const location = useLocation();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
+  const [page, setPage] = useState(1);
 
   const searchParams = new URLSearchParams(location.search);
   const title = useDebounce(searchParams.get('title') || '', 1000);
@@ -31,17 +34,41 @@ function MoviesPage() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
+    setMovies([]);
+    setPage(1);
+  }, [year, title]);
+
+  useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true);
       try {
-        const { data } = await getMovies(year, title, 1);
-        setMovies(data);
+        const { data } = await getMovies(year, title, page);
+        setMovies((prevMovies) => [...prevMovies, ...data]);
+        setHasMore(data.length > 0);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
       }
     };
 
     fetchMovies();
-  }, [year, title]);
+  }, [year, title, page]);
+
+  const observer = useRef();
+  const lastMovieElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   const handleMovieClick = (movieID) => {
     navigate(`/movies/data/${movieID}`);
@@ -50,14 +77,28 @@ function MoviesPage() {
   return (
     <div className="movies-page">
       <div className="movie-grid">
-        {movies.map((movie) => (
-          <MovieCell
-            key={movie.imdbID}
-            movie={movie}
-            onClick={() => handleMovieClick(movie.imdbID)}
-          />
-        ))}
+        {movies.map((movie, index) => {
+          if (movies.length === index + 1) {
+            return (
+              <div ref={lastMovieElementRef} key={movie.imdbID}>
+                <MovieCell
+                  movie={movie}
+                  onClick={() => handleMovieClick(movie.imdbID)}
+                />
+              </div>
+            );
+          } else {
+            return (
+              <MovieCell
+                key={movie.imdbID}
+                movie={movie}
+                onClick={() => handleMovieClick(movie.imdbID)}
+              />
+            );
+          }
+        })}
       </div>
+      <div>{loading && 'Loading...'}</div>
     </div>
   );
 }
